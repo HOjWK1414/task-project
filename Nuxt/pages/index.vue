9583476227f22a133c332b1de6489def3f4989de
kocur <kocur@tsoftas.onmicrosoft.com>
2025-05-27 21:30:26 +0200

    adj: login onLoading disable form

--- Nuxt/pages/index.vue
+++ Nuxt/pages/index.vue
@@ -18,16 +18,22 @@
                             <p class="text-muted-foreground text-sm">{{ PageModel.form_description }}</p>
                         </div>
 
-                        <Input
+                        <Input :disabled="isLoading"
                             class="border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                             v-model="email" type="email" :placeholder="PageModel.form_email_placeholder" required />
-                        <Input
+
+                        <Input :disabled="isLoading"
                             class="border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                             v-model="password" type="password" :placeholder="PageModel.form_psw_placeholder" required />
 
-                        <Button type="submit"
+                        <Button type="submit" :disabled="isLoading"
                             class="w-full ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2">
-                            {{ PageModel.form_submit }}
+                            <svg v-if="isLoading" class="animate-spin mr-2 h-4 w-4 text-white"
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" fill="none">
+                                <circle cx="25" cy="25" r="20" stroke="currentColor" stroke-width="5"
+                                    stroke-linecap="round" stroke-dasharray="90" stroke-dashoffset="30" />
+                            </svg>
+                            <span>{{ PageModel.form_submit }}</span>
                         </Button>
 
                         <p class="text-muted-foreground px-8 text-center text-sm" v-html="PageModel.form_notice"></p>
@@ -53,8 +59,13 @@ const router = useRouter()
 const email = ref('')
 const password = ref('')
 
+const isLoading = ref(false)
+
 const handleLogin = async () => {
+    isLoading.value = true
     const success = await useAuth(email.value, password.value)
+    isLoading.value = false
+
     if (success) {
         router.push('/dashboard')
     }
